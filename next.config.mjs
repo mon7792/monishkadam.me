29b3b7f438b73ac24885fd48aaed5efd7c49a5ee
mon7792 <monish7792@gmail.com>
2024-11-17 19:01:05 +0000

    feat: next js ssg enabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: "dist",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
