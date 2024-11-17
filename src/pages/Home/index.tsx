3ba91fd55a96501e2e29e5ca01707c8f19817b4e
mon7792 <monish7792@gmail.com>
2024-11-17 14:35:41 +0000

    feat: sample home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+	return (
+		<section className="flex flex-col h-screen justify-center items-center">
+			<div className="text-xl font-bold">Hey, Welcome <span className="text-4xl font-bold">👋</span></div>
+			<div>
+				<span className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+					Monish
+				</span>
+				<span className="text-lg font-bold">, here</span>
+			</div>
+		</section>
+	);
+};
+
+export default HomePage;
